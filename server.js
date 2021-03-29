53ddd4d9661b9b1a3a75d4c7a867584910cbf4bc
Gabriel Lopes <57074487+unifgabsantos@users.noreply.github.com>
2021-03-29 13:00:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,48 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const SECRET_KEY = require('./secret')
+const { response } = require('express');
 var server = express();
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 server.use('/', express.static('Arquivos'));
 server.set("view engine", "ejs");
 var dados = [];
-
-function EnviarDados(req, resp) {
-    var info = {
-        "Maquina": req.body.Maquina,
-        "Dados": {
-            "Time": req.body.Time,
-            "Temperatura": req.body.Temperatura
-        }
-    };
-    dados.push(info);
-    resp.send({"Status_Code":200})
-    console.log(info);
-};
+var info;
 
 function GetDados(req, resp) {
     resp.setHeader("Access-Control-Allow-Origin", "*");
     resp.send(dados);
 };
 
+function PostDados(req, resp) {
+    if (req.query.Senha == SECRET_KEY) {
+        info = { "Temperatura": req.query.Temperatura, "Time": new Date() }
+        dados.push(info);
+        resp.send({ "Status": 200 });
+    } else {
+        resp.status(401);
+        resp.send({ "Erro": "Senha Incorreta" });
+    }
+    console.log(info);
+};
+
+function DeleteDados(req, resp) {
+    if (req.query.Senha == SECRET_KEY) {
+        dados = [];
+    }
+    else{
+        resp.status(401);
+        resp.send({ "Erro": "Senha Incorreta" });
+    }
+};
+
 function Index(req, resp) {
     resp.render("index");
 };
 
-function Clear(req,resp){
-    if (req.body.Password=="admin"){
-        dados = [];
-        resp.send("Clear");
-    }
-    else resp.send("Senha incorreta.")
-};
-
-
-server.post("/Enviar", EnviarDados);
-server.post("/Clear",Clear);
-server.get("/GetDados", GetDados);
 server.get("/", Index);
+server.get("/Receber", GetDados);
+server.post("/Enviar", PostDados);
+server.post("/Deletar", DeleteDados);
 server.listen(80);
